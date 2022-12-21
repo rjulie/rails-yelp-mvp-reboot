895a1165f6f688f11a445274440be4510118caf6
Julie Reynal <rjulie.pro@gmail.com>
2022-12-21 16:25:18 +0100

    rails yelp mvp advanced routing day

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES } # inclusion: { in: %w[chinese italian japanese french belgian] }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+friterie = { name: "La Friterie", address: "Bruxelles", category: "belgian" }
+joe = { name: "Joe's Shangai ", address: "New York", category: "chinese" }
+ober =  { name: "Ober Mamma", address: "Paris", category: "italian" }
+ichiban = { name: "Ichiban Sushi", address: "Limoges", category: "japanese" }
+parfum = { name: "Un parfum de gourmandise", address: "Périgueux", category: "french" }
+
+[friterie, joe, ober, ichiban, parfum].each do |attributes|
+  restaurant = Restaurant.new(attributes)
+  restaurant.save!
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
